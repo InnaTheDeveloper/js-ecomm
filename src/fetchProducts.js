3d2054fca6f0956b84786c2615ad6dc0274666c5
InnaTheDeveloper <93871719+InnaTheDeveloper@users.noreply.github.com>
2024-05-17 15:25:58 +0300

    cleaned console.log
    cleaned console.log

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -2,7 +2,6 @@ import { allProductsUrl } from './utils.js';
 
 const fetchProducts = async () => {
   const response = await fetch(allProductsUrl).catch((err) => console.log(err));
-  //   console.log(response);
   if (response) {
     return response.json();
   }
